b128c3d507454b6cfc95f3faf4964158ffa96396
AndersonMarques0 <119546595+AndersonMarques0@users.noreply.github.com>
2024-09-14 00:34:35 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -51,6 +51,23 @@ const listarMetas = async () => {
 
 }
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas! :(')
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+
+}
+
 const start = async () => {
     
     while(true){
@@ -67,6 +84,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -81,6 +102,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima")
